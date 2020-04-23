90d8788b76a6404426ee2ae0a6e96ba78942ee10
Itachi <loyal.lugia@gmail.com>
2020-04-23 12:43:20 -0400

    Update TameshiEng.js

--- TameshiEng.js
+++ TameshiEng.js
@@ -1,16 +1,44 @@
 const Discord = require("discord.js")
 const bot = new Discord.Client();
-
-const {Token, WebhookID, WebhookToken} = require('./auth.json');
+const { token, prefix, webhook0, webhook1 } = require('./auth.json');
 
 bot.once('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
     console.log("Bot has started")
 });
 
-const webhook = new Discord.WebhookClient (WebhookID, WebhookToken);
 
-bot.login (Token);
 
-webhook.send ("Hello World!");
+var webhookID = [webhook0.ID, webhook1.ID];
+var webhookToken = [webhook0.token, webhook1.token];
+
+
+bot.on("message", async message => {
+
+    var announcement = message.content
+    console.log(announcement);
+
+
+
+
+
+    if (announcement.charAt(0)==prefix) {
+
+        announcement = announcement.substring(prefix.length, (announcement.length)-1)
+
+        for (i = 0; i < webhookID.length - 1; i++) {
+            let webhook = new Discord.WebhookClient(webhookID[i], webhookToken[i])
+            webhook.send(announcement);
+        }
+        console.log ("announcement has been sent")
+    }
+});
+
+
+bot.login(token);
+
+
+
+
+
 
