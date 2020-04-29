615c3498a089c593edbe7fe1f4a0450d656cb93a
Itachi <loyal.lugia@gmail.com>
2020-04-29 00:02:55 -0400

    Update TameshiEng.js
    Fixed array sizing

--- TameshiEng.js
+++ TameshiEng.js
@@ -1,33 +1,28 @@
 const Discord = require("discord.js")
-const bot = new Discord.Client();
-const auth = require('./auth.json');
-const id = require('./id.json');
-const token = require('./token.json');
+const auth = require('./auth.json')
+const id = require('./id.json')
+const token = require('./token.json')
+
+const bot = new Discord.Client()
+const idArray = JSON.stringify (id)
+const tokenArray = JSON.stringify (token)
 
 bot.once('ready', () => {
-    console.log(`Logged in as ${bot.user.tag}!`);
+    console.log(`Logged in as ${bot.user.tag}!`)
     console.log("Bot has started")
 });
 
 bot.on("message", async message => {    
     var announcement = message.content
     if (message.author.bot) {
-        return;
+        return
     }
     else if (message.guild.id == auth.hostGuildID) {
         if (announcement.charAt(0) == auth.prefix) {
-            const webhook0 = new Discord.WebhookClient(id.id0, token.token0);
-            webhook0.send(announcement);
-
-            const webhook1 = new Discord.WebhookClient(id.id1, token.token1);
-            webhook1.send(announcement);
-
-            const webhook2 = new Discord.WebhookClient(id.id2, token.token2);
-            webhook2.send(announcement);
+            for (i = 0; i<idArray.length; i++)
+            var webhook = new Discord.WebhookClient(idArray[i], tokenArray[i])
+            webhook.send(announcement)
         }
-
     }
 });
-
-
 bot.login(auth.token);
