240a5995ef2f7c8f7a6719e09c56983cc1ca6472
Itachi <loyal.lugia@gmail.com>
2020-05-01 14:52:06 -0400

    Update TameshiEng.js

--- TameshiEng.js
+++ TameshiEng.js
@@ -4,8 +4,14 @@ const id = require('./id.json')
 const token = require('./token.json')
 
 const bot = new Discord.Client()
+
 const idArray = JSON.stringify (id)
 const tokenArray = JSON.stringify (token)
+
+const idArray = new Array(id);
+const tokenArray = new Array(token);
+
+
 
 bot.once('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`)
@@ -18,10 +24,17 @@ bot.on("message", async message => {
         return
     }
     else if (message.guild.id == auth.hostGuildID) {
-        if (announcement.charAt(0) == auth.prefix) {
-            for (i = 0; i<idArray.length; i++)
-            var webhook = new Discord.WebhookClient(idArray[i], tokenArray[i])
-            webhook.send(announcement)
+
+        try {
+            if (announcement.charAt(0) == auth.prefix) {
+                for (i = 0; i < 2; i++)
+                    var webhook = new Discord.WebhookClient(idArray[i], tokenArray[i])
+                console.log(idArray[i])
+                webhook.send(announcement)
+            }
+        }
+        catch {
+
         }
     }
 });
