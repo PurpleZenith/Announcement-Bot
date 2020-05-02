51b7a59cae076cf0b2cc874de52f65641cfdc46c
Itachi <loyal.lugia@gmail.com>
2020-05-02 14:39:03 -0400

    Update TameshiEng.js
    Import embed for webhook

--- TameshiEng.js
+++ TameshiEng.js
@@ -2,11 +2,14 @@ const Discord = require("discord.js")
 const auth = require('./auth.json')
 const id = require('./id.json')
 const token = require('./token.json')
+const embed = require('./embed.js')
+
 
 const bot = new Discord.Client()
-
 const idArray = jsonToArray(id)
 const tokenArray = jsonToArray(token)
+
+const richEmbed = embed.embed
 
 function jsonToArray(reqJSON) {
     const str = JSON.stringify(reqJSON)
@@ -18,9 +21,9 @@ function jsonToArray(reqJSON) {
 
 bot.once('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`)
-    console.log("Bot has started")
+    console.log("Embed:")
+    console.log(embed)
 });
-
 
 bot.on("message", async message => {
     var announcement = message.content
@@ -28,17 +31,14 @@ bot.on("message", async message => {
         return
     }
     else if (message.guild.id == auth.hostGuildID) {
-
         if (announcement.charAt(0) == auth.prefix) {
-
-
-            for (i = 0; i < idArray.length; i++) {
+            for (i = 0; i < 2; i++) {
                 var webhook = new Discord.WebhookClient(idArray[i], tokenArray[i])
-                console.log (idArray[i])
-                webhook.send(announcement)
+                console.log(idArray[i])
+                webhook.send("", { embeds: [richEmbed] })
             }
-
         }
     }
 });
 bot.login(auth.token);
+
